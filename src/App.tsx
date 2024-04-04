92b202be7bcc2b0d62b6756c5c6043fa59cdaa2e
FedorovAD <alex_fed02@mail.ru>
2024-04-04 20:29:24 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button, Flex, Text } from '@chakra-ui/react';
 
 function App() {
+  const [counter, setCounter] = useState(0)
+  useEffect(() => {
+    setCounter(10)
+  }, [])
   return (
-    <div className="App">
-      hello world
-    </div>
+    <Flex className="App" direction={'column'} width={"100vw"} height={"100vh"} justifyContent={'center'} alignItems={'center'}>
+      <Text>
+        {counter}
+      </Text>
+      <Flex gap={'3'}>
+        <Button onClick={() => setCounter((prev) => prev - 1)} width={'30px'} height={'20px'} variant={'outline'} colorScheme='blue'>
+            -
+        </Button>
+        <Button onClick={() => setCounter((prev) => prev + 1)} width={'30px'} height={'20px'} variant={'outline'} colorScheme='red'>
+            +
+        </Button>
+      </Flex>
+    </Flex>
   );
 }
 
