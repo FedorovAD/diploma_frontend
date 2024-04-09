152c5c791e87950934cd0dee7bacb29276928eff
FedorovAD <alex_fed02@mail.ru>
2024-04-10 01:25:19 +0300

    smth

---
+++ src/pages/result-page.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { 
+  Box, 
+  Button, 
+  Flex, 
+  Text, 
+  useDisclosure,
+  Card,
+  CardHeader, 
+  CardBody, 
+  CardFooter,
+  Input, 
+  InputLeftAddon, 
+  InputRightAddon, 
+  Stack, 
+  InputGroup,
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderMark,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Heading
+} from '@chakra-ui/react'
+
+
+
+
+
+function ResultPage() {
+  
+  return (
+    <Box>
+
+    </Box> 
+  );
+}
+
+export default ResultPage;
